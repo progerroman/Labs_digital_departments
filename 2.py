df7f28838d9d20f2f536b1d87e20bf695d006e53
progerroman <121955861+progerroman@users.noreply.github.com>
2024-11-10 23:59:38 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,13 +1,10 @@
 def find_common_participants(group1, group2, separator=','):
+    participants1 = set(group1.split(separator))
+    participants2 = set(group2.split(separator))
 
-    participants1 = group1.split(separator)
-    participants2 = group2.split(separator)
+    common_participants = sorted(participants1.intersection(participants2))
 
-
-    common_participants = list(set(participants1) & set(participants2))
-
-
-    return sorted(common_participants)
+    return common_participants
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
@@ -15,5 +12,5 @@ participants_second_group = "Петров|Сидоров|Смирнов"
 
 common_participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
 
-
 print("Общие участники:", common_participants)
+
